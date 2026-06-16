599b69cb6ee72f273eca9bbd391106d1fdb0a609
pixelguy13 <13hritrock@gmail.com>
2026-06-16 14:58:04 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,3 +13,10 @@ export default defineConfig({
     server: { entry: "server" },
   },
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+})
